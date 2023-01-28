256730583b69d4f7c6369da4752725ac17a5490f
imperialbyte <110067750+imperialbyte@users.noreply.github.com>
2023-01-28 21:57:17 +1000

    Update russian_roulette_cplusplus.cpp

--- russian_roulette_cplusplus.cpp
+++ russian_roulette_cplusplus.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <limits>
+#include <Windows.h>
+#include <thread>
+
+// russian roulette version 2
+
 
 const int MAX_ROUNDS = 6;
+
+std::string print_animation(std::string text);
 
 int option;
 
@@ -12,38 +19,64 @@ bool alive = true;
 
 void spin_chamber()
 {
-    active_chamber = rand() % MAX_ROUNDS;
+    active_chamber = rand() % MAX_ROUNDS + 1;
     current_chamber = 1;
-    std::cout << "You spun the chamber!" << std::endl;
+    print_animation("You spun the chamber!");
+    std::cout << std::endl;
 }
 
 void shoot()
 {
     if(current_chamber == active_chamber)
     {
-        std::cout << "You are dead." << std::endl;
+        std::cout << "***BANG!***" << std::endl;
+        Sleep(1000);
+        print_animation("You are dead!");
+        std::cout << std::endl;
         std::cout << "You clicked the trigger " << score << " time(s) before you died" << std::endl;
         current_chamber++;
         alive = false;
     } else {
-        std::cout << "You are alive!!!" << std::endl;
+        std::cout << "**CLICK!**" << std::endl;
+        Sleep(1000);
+        print_animation("Nothing happened...");
+        std::cout << std::endl;
         score++;
         current_chamber++;
     }
 }
 
 void reveal_chamber() {
-    std::cout << "The bullet is at chamber " << active_chamber << std::endl;
-    std::cout << "You are at chamber " << current_chamber << std::endl;
+    print_animation("The bullet is at chamber "); std::cout<< active_chamber; print_animation("...");
+    Sleep(500);
+    std::cout << std::endl;
+    print_animation("Your gun is at chamber "); std::cout<< current_chamber; print_animation("...");
+    std::cout << std::endl;
+
+    if(current_chamber == active_chamber) {
+        print_animation("You will be killed if you pull the trigger!");
+        std::cout << std::endl;
+    }
 }
 
 int main() {
     srand(time(NULL));
 
-    std::cout << "****************" << std::endl;
-    std::cout << "RUSSIAN ROULETTE" << std::endl;
-    std::cout << "****************" << std::endl;
-    std::cout << "You have a revolver,\nwhat would you like to do?" << std::endl;
+    print_animation("****************");
+    std::cout << std::endl;
+    print_animation("RUSSIAN ROULETTE");
+    std::cout << std::endl;
+    print_animation("****************");
+    std::cout << std::endl;
+    Sleep(1000);
+    print_animation("WELCOME TO RUSSIAN ROULETTE!");
+    std::cout << std::endl;
+    Sleep(500);
+    print_animation("There are no winners here...");
+    std::cout << std::endl;
+    Sleep(1000);
+    print_animation("You have a revolver,\nwhat would you like to do?");
+    std::cout << std::endl;
 
     active_chamber = rand() % MAX_ROUNDS + 1;
 
@@ -58,7 +91,8 @@ int main() {
         std::cout << std::endl;
 
         if(std::cin.fail() || option > 4 || option < 1 || std::cin.fail()) {
-            std::cout << "Invalid input, please enter a number between 1 and 4." << std::endl;
+            print_animation("Invalid input, please enter a number between 1 and 4.");
+            std::cout << std::endl;
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             // clear the input buffer and ignore any other input until newline character is reached
@@ -76,18 +110,24 @@ int main() {
                     reveal_chamber();
                     break;
                 case 4:
-                    std::cout << "*******************" << std::endl;
-                    std::cout << "YOU DECIDED TO QUIT" << std::endl;
-                    std::cout << "*******************" << std::endl;
+                    print_animation("You decided to quit!");
                     return 0;
                 default:
                     std::cout << "How did you even get this error?" << std::endl;
                     break;;
             }
         }
-
     } while (alive);
     
 
     return 0;
+}
+
+std::string print_animation(std::string text)
+{
+    for (int i = 0; i < text.length(); i++) {
+        std::cout << text[i];
+        Sleep(50);
+    }
+    return "";
 }
